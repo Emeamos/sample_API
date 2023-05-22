5d6c461d25e64e0af637ca5cff19c18af33c0fec
Emeamos <Emediongamos.engr@gmail.com>
2023-05-22 07:50:03 +0100

    updating comment route

--- route/commentsRoute.js
+++ route/commentsRoute.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { getComments } from "../controller/commentsController";
+import { getComments } from "../controller/commentsController.js";
 const commentsRouter = express.Router();
 
 commentsRouter.get('/', getComments);
