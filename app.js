6af27c5dd8eb13108caf11177c89c95a6fe1f642
Emeamos <Emediongamos.engr@gmail.com>
2023-05-22 07:51:40 +0100

    adding routes to app.js

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 import express from "express";
+import commentsRouter from "./route/commentsRoute.js";
+import postsRouter from "./route/postsRoute.js";
 
 const app = express();
+
+app.use('/api/comments', commentsRouter);
+app.use('/api/posts', postsRouter);
 
 const PORT = process.env.PORT || 7007
 app.listen(PORT,console.log(`Server is running at ${PORT}`))
